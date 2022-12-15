dd39aaa28e5f649a7e8e9e12216fac09a334ef50
cory <1253601435@qq.com>
2022-12-15 10:42:22 +0800

    trigger the first build

--- src/main/java/org/example/SpringbootApplication.java
+++ src/main/java/org/example/SpringbootApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
-        System.out.println("hello cory");
+        System.out.println("hello cory!");
     }
 }
